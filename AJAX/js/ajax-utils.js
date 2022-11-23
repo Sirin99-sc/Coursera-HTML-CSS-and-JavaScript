b4f309b2b655388080695cc757dc5eaea7866d6b
Sirin <shirinchakraborty0@gmail.com>
2022-11-23 12:49:29 +0530

    assignment 5 done

--- AJAX/js/ajax-utils.js
+++ AJAX/js/ajax-utils.js
@@ -1,3 +1,4 @@
+/*Ajax intro
 (function (global) {
 	
 	var ajaxUtils={};
@@ -32,6 +33,58 @@
      (request.status == 200)) {
     responseHandler(request);
   }
+}
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+})(window);*/
+
+/*Json*/
+(function (global) {
+	
+	var ajaxUtils={};
+
+	function getRequestObject(){
+		if (global.XMLHttpRequest) {
+			//for current versions
+			return (new XMLHttpRequest());
+		}
+		else if(global.ActiveXObject){
+			//for old browers
+			return(new ActiveXObject("Microsoft.XMLHTTP"));
+		}
+		else{
+			global.alert("Ajax is notsupported!");
+			return(null);
+		}
+	} 
+
+	ajaxUtils.sendGetRequest=
+	function(requestUrl,responseHandler,isJsonResponse){
+		var request=getRequestObject();
+		request.onreadystatechange=function(){
+			handleResponse(request,responseHandler,isJsonResponse);
+		};
+		request.open("GET",requestUrl,true);//true for async req
+		request.send(null);//for POST only
+	};
+
+	function handleResponse(request,
+                        responseHandler,isJsonResponse) {
+  if ((request.readyState == 4) &&
+     (request.status == 200)) {
+
+  	   if (isJsonResponse==undefined) {
+  	   	isJsonResponse=true;
+  	   }
+
+  	   if (isJsonResponse) {
+  	   	responseHandler(JSON.parse(request.responseText));
+  	   }
+       else{
+    responseHandler(request.responseText);
+  }
+}
 }
 // Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
--- AJAX/js/script.js
+++ AJAX/js/script.js
@@ -1,4 +1,5 @@
 // Event handling
+/*Ajax
 document.addEventListener("DOMContentLoaded",//will execute when page loaded
   function (event) {
     
@@ -19,4 +20,37 @@ document.addEventListener("DOMContentLoaded",//will execute when page loaded
         
       });
   }
+);*/
+
+/*Json*/
+document.addEventListener("DOMContentLoaded",//will execute when page loaded
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var mssg=res.firstName+" "+res.lastName
+              if (res.likesChineseFood) {
+                mssg+=" likes Chinese food";
+              }
+              else{
+                mssg+=" doesn't like Chinese food";
+              }
+              mssg+=" and uses ";
+              mssg+=res.numberOfDisplays+1;
+              mssg+=" displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" +mssg + "</h2>";
+            });
+
+        
+      });
+  }
 );
+
